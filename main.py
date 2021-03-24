2da014c40d1a3527962f326aaf10157d87141fc9
VictorCoutinho86 <victormelo@id.uff.br>
2021-03-24 10:10:18 -0300

    Adicionando busca pela posição do mouse

--- main.py
+++ main.py
@@ -5,5 +5,8 @@ import time
 pyautogui.FAILSAFE = False
 while True:
     print("Movendo mouse...")
-    pyautogui.moveTo(random.randint(0, 800), random.randint(0,600))
+    pyautogui.moveTo(random.randint(799, 800), random.randint(599,600))
+    time.sleep(1)
+    print("Clicando no local...")
+    pyautogui.click()
     time.sleep(30)
---
+++ position.py
@@ -0,0 +1,20 @@
+import random
+import time
+
+import pyautogui
+
+pyautogui.FAILSAFE = False
+posicao = pyautogui.position()
+
+try:
+    while True:
+        if posicao == pyautogui.position():
+            print("Movendo mouse...")
+            pyautogui.moveTo(random.randint(799, 800), random.randint(599,600))
+            print("Clicando no local...")
+            pyautogui.click()
+        print("Atualizando posição do mouse...")
+        posicao = pyautogui.position()
+        time.sleep(30)
+except KeyboardInterrupt:
+    print("Parando execução...")
