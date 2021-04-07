4c6a7e32a81c069ee1b4a04f4a38d4d61cd59276
VictorCoutinho86 <victormelo@id.uff.br>
2021-04-07 15:39:19 -0300

    Obtendo variável de ambiente

--- position.py
+++ position.py
@@ -1,3 +1,4 @@
+import os
 import random
 import time
 
@@ -10,11 +11,12 @@ try:
     while True:
         if posicao == pyautogui.position():
             print("Movendo mouse...")
-            pyautogui.moveTo(random.randint(799, 800), random.randint(599,600))
+            pyautogui.moveTo(random.randint(799, 800),
+                             random.randint(599, 600))
             print("Clicando no local...")
             pyautogui.click()
         print("Atualizando posição do mouse...")
         posicao = pyautogui.position()
-        time.sleep(30)
+        time.sleep(os.getenv("TIMER", 90))
 except KeyboardInterrupt:
     print("Parando execução...")
